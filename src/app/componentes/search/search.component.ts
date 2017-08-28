a944f13e75212aa31ee5fd198a5865deefbd2dcb
darz0212 <30479712+darz0212@users.noreply.github.com>
2017-08-28 15:59:25 -0500

    Modulo 2
    Añado las busquedas por nombre y por cocción

--- src/app/componentes/search/search.component.ts
+++ src/app/componentes/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Recipe } from '../../interfaces/recipe';
+import { MockupRecipeService } from '../../services/mockup-recipe.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  recipe: Recipe;
+  recipes: Recipe[] = [];
+  recipesSearched: Recipe[] = [];
+  ingredientes: String[] = [];
+  
+  showRecipe: boolean;
+  constructor(private _mockup: MockupRecipeService) {
+    this.recipes = this._mockup.getRecipes();
+    this.showRecipe = false;
+   }
 
-  constructor() { }
+   showRecipes(){
+    this.showRecipe = !this.showRecipe;
+   }
+   searchRecipeName(searchName){
+      this.recipesSearched=[];
+      for (var i = 0; i < this.recipes.length; i++) {
+        if (this.recipes[i].name === searchName.value) {
+        this.recipesSearched.push(this.recipes[i]);
+        }      
+      }
+    this.showRecipe = true;        
+   }
+
+   searchRecipeTime(searchTime){
+     console.log(searchTime.value);
+    this.recipesSearched=[];
+    for (var i = 0; i < this.recipes.length; i++) {
+      if (this.recipes[i].time === parseInt(searchTime.value)) {
+      this.recipesSearched.push(this.recipes[i]);
+      }      
+    }
+  this.showRecipe = true;     
+   }
+
+
 
   ngOnInit() {
   }
 
 }
+
